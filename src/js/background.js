13334e229d411a91d0fbd016569edb17c60047fa
jfxo0 <janisha.ly.wong@gmail.com>
2025-06-01 02:18:31 +0200

    final touches

--- src/js/background.js
+++ src/js/background.js
@@ -2,20 +2,20 @@ import { Actor, Sprite, Vector } from "excalibur"
 import { Resources } from "./resources"
 
 export class Background extends Actor {
-    sprite;
+    #sprite;
 
     onInitialize(engine) {
-        this.sprite = new Sprite({
+        this.#sprite = new Sprite({
             image: Resources.Bg,
             sourceView: { x: 0, y: 250, width: engine.drawWidth, height: engine.drawHeight }
         })
         this.anchor = Vector.Zero
-        this.graphics.use(this.sprite)
+        this.graphics.use(this.#sprite)
 
     }
 
     onPostUpdate(engine, delta) {
-        this.sprite.sourceView.x += .05 * delta;
+        this.#sprite.sourceView.x += .05 * delta;
     }
 
 
